2cfa13d8c7d8f56cf3d4f08d9ca6c2b5b42f9f4d
khmuhtadin <khmuhtadin@student.untan.ac.id>
2023-10-26 01:14:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,41 +11,14 @@ function selectProduct(productElement) {
     }
 }
 
-function checkout() {
+function proceedToInvoice() {
     if (!selectedProduct) {
         alert("Silakan pilih produk terlebih dahulu.");
         return;
     }
 
     totalAmount = selectedProduct.price * selectedProduct.quantity;
-    const orderDetailElement = document.getElementById("orderDetails");
-    orderDetailElement.innerHTML = `
-        <h2>Detail Pesanan</h2>
-        Nama Produk: ${selectedProduct.name}<br>
-        Harga Satuan: Rp${selectedProduct.price.toLocaleString()}<br>
-        Jumlah: ${selectedProduct.quantity}<br>
-        Total Harga: Rp${totalAmount.toLocaleString()}<br>
-        <button onclick="generateInvoice()">Buat Invoice</button>
-    `;
-}
-
-function generateInvoice() {
-    const uniqueDigits = Math.floor(100 + Math.random() * 900);
-    const finalPayment = totalAmount + uniqueDigits;
-    const invoiceNumber = "INV" + new Date().getTime();
-
-    const invoiceDetailElement = document.getElementById("invoiceDetails");
-    invoiceDetailElement.innerHTML = `
-        <h2>Invoice</h2>
-        Nomor Invoice: ${invoiceNumber}<br>
-        Total Pembayaran (termasuk 3 digit unik): Rp${finalPayment.toLocaleString()}<br>
-        Cara Pembayaran: Transfer
-    `;
-
-    // Kirim event ke GA melalui GTM
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-        'event': 'generate_invoice',
-        'value': finalPayment
-    });
+    localStorage.setItem("selectedProduct", JSON.stringify(selectedProduct));
+    localStorage.setItem("totalAmount", totalAmount.toString());
+    window.location.href = "invoice.html";
 }
