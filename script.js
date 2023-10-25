d48f0fe9e5ef5a96fb732f026e1d234295857cb0
khmuhtadin <khmuhtadin@student.untan.ac.id>
2023-10-26 01:10:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,51 @@
-let cart = [];
-const uniqueDigits = Math.floor(Math.random() * 899) + 100;
+let selectedProduct = null;
+let quantity = 0;
+let totalAmount = 0;
 
-function hideAll() {
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('checkout').style.display = 'none';
-    document.getElementById('orderDetail').style.display = 'none';
-    document.getElementById('invoice').style.display = 'none';
+function selectProduct(productElement) {
+    const name = productElement.getAttribute("data-name");
+    const price = parseFloat(productElement.getAttribute("data-price"));
+    quantity = prompt("Masukkan jumlah untuk " + name, "1");
+    if (quantity) {
+        selectedProduct = { name, price, quantity: parseInt(quantity) };
+    }
 }
 
 function checkout() {
-    hideAll();
-    populateProducts();
-    document.getElementById('checkout').style.display = 'block';
-}
-
-function populateProducts() {
-    const products = [
-        { name: "Produk Alpha", price: 1260000 },
-        { name: "Produk Beta", price: 1250000 },
-        { name: "Produk Gamma", price: 1245000 }
-    ];
-    
-    const productList = document.getElementById('productList');
-    productList.innerHTML = '';
-    
-    products.forEach(product => {
-        const productDiv = document.createElement('div');
-        productDiv.classList.add('product');
-        productDiv.innerHTML = `
-            <h3>${product.name}</h3>
-            <p>Harga: IDR ${product.price.toLocaleString()}</p>
-            <input type="number" value="1" id="quantity-${product.name}" min="1"/>
-            <button onclick="addToCart('${product.name}', ${product.price})">Tambah ke Keranjang</button>
-        `;
-        productList.appendChild(productDiv);
-    });
-}
-
-function addToCart(productName, productPrice) {
-    const quantity = parseInt(document.getElementById(`quantity-${productName}`).value);
-
-    const existingProduct = cart.find(item => item.name === productName);
-    if (existingProduct) {
-        existingProduct.quantity += quantity;
-    } else {
-        cart.push({
-            name: productName,
-            price: productPrice,
-            quantity: quantity
-        });
+    if (!selectedProduct) {
+        alert("Silakan pilih produk terlebih dahulu.");
+        return;
     }
-    alert(`${productName} telah ditambahkan ke keranjang dengan jumlah ${quantity}.`);
-}
 
-function orderDetail() {
-    hideAll();
-    populateOrderDetails();
-    document.getElementById('orderDetail').style.display = 'block';
-}
-
-function populateOrderDetails() {
-    const detailsDiv = document.getElementById('orderDetailsList');
-    detailsDiv.innerHTML = "";
-    
-    let total = 0;
-    cart.forEach(item => {
-        detailsDiv.innerHTML += `<p>${item.name} - IDR ${item.price.toLocaleString()} x ${item.quantity} = IDR ${(item.price * item.quantity).toLocaleString()}</p>`;
-        total += item.price * item.quantity;
-    });
-    
-    document.getElementById('totalPayment').innerText = total.toLocaleString();
+    totalAmount = selectedProduct.price * selectedProduct.quantity;
+    const orderDetailElement = document.getElementById("orderDetails");
+    orderDetailElement.innerHTML = `
+        <h2>Detail Pesanan</h2>
+        Nama Produk: ${selectedProduct.name}<br>
+        Harga Satuan: Rp${selectedProduct.price.toLocaleString()}<br>
+        Jumlah: ${selectedProduct.quantity}<br>
+        Total Harga: Rp${totalAmount.toLocaleString()}<br>
+        <button onclick="generateInvoice()">Buat Invoice</button>
+    `;
 }
 
 function generateInvoice() {
-    hideAll();
+    const uniqueDigits = Math.floor(100 + Math.random() * 900);
+    const finalPayment = totalAmount + uniqueDigits;
+    const invoiceNumber = "INV" + new Date().getTime();
 
-    const invoiceNum = `INV${new Date().getTime()}`;
-    document.getElementById('invoiceNumber').innerText = invoiceNum;
+    const invoiceDetailElement = document.getElementById("invoiceDetails");
+    invoiceDetailElement.innerHTML = `
+        <h2>Invoice</h2>
+        Nomor Invoice: ${invoiceNumber}<br>
+        Total Pembayaran (termasuk 3 digit unik): Rp${finalPayment.toLocaleString()}<br>
+        Cara Pembayaran: Transfer
+    `;
 
-    let total = 0;
-    cart.forEach(product => {
-        total += product.price * product.quantity;
-    });
-
-    const finalPayment = total + uniqueDigits;
-    document.getElementById('finalPayment').innerText = finalPayment.toLocaleString();
-    
-    document.getElementById('invoice').style.display = 'block';
-}
-
-function generateInvoice() {
-    ...
-    // Tambahkan kode berikut untuk mengirimkan event ke GA4
+    // Kirim event ke GA melalui GTM
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
         'event': 'generate_invoice',
         'value': finalPayment
     });
-    ...
 }
