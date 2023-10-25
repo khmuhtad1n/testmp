2f2866f45e2caba1161bc2920d3fe8f6064e72f4
khmuhtadin <khmuhtadin@student.untan.ac.id>
2023-10-26 00:38:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-function checkout() {
-    hideAll();
-    populateProducts();
-    document.getElementById('checkout').style.display = 'block';
-}
-
-function populateProducts() {
-    const productList = document.getElementById('productList');
-    for(let i=0; i<3; i++) {
-        const productName = generateRandomProductName();
-        const productPrice = generateRandomProductPrice();
-        
-        const productDiv = document.createElement('div');
-        productDiv.classList.add('product');
-        productDiv.innerHTML = `
-            <h3>${productName}</h3>
-            <p>Harga: IDR ${productPrice.toLocaleString()}</p>
-            <button onclick="addToCart('${productName}', ${productPrice})">Tambah ke Keranjang</button>
-        `;
-        productList.appendChild(productDiv);
-    }
-}
-
-function orderDetail() {
-    hideAll();
-    document.getElementById('orderDetail').style.display = 'block';
-}
-
-function generateInvoice() {
-    hideAll();
-    document.getElementById('invoice').style.display = 'block';
-}
+let cart = [];
+let uniqueDigits = Math.floor(Math.random() * 899) + 100;
 
 function hideAll() {
     document.getElementById('home').style.display = 'none';
@@ -38,15 +8,70 @@ function hideAll() {
     document.getElementById('invoice').style.display = 'none';
 }
 
-function generateRandomProductName() {
-    const names = ["Produk Alpha", "Produk Beta", "Produk Gamma"];
-    return names[Math.floor(Math.random() * names.length)];
+function checkout() {
+    hideAll();
+    populateProducts();
+    document.getElementById('checkout').style.display = 'block';
 }
 
-function generateRandomProductPrice() {
-    return Math.floor(Math.random() * (1276500 - 124000 + 1)) + 124000;
+function populateProducts() {
+    const products = [
+        { name: "Produk Alpha", price: generateRandomProductPrice() },
+        { name: "Produk Beta", price: generateRandomProductPrice() },
+        { name: "Produk Gamma", price: generateRandomProductPrice() }
+    ];
+    const productList = document.getElementById('productList');
+    productList.innerHTML = "";
+    products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product');
+        productDiv.innerHTML = `
+            <h3>${product.name}</h3>
+            <p>Harga: IDR ${product.price.toLocaleString()}</p>
+            <input type="number" value="1" id="quantity-${product.name}"/>
+            <button onclick="addToCart('${product.name}', ${product.price})">Tambah ke Keranjang</button>
+        `;
+        productList.appendChild(productDiv);
+    });
 }
 
 function addToCart(productName, productPrice) {
-    console.log(`Produk ${productName} dengan harga IDR ${productPrice.toLocaleString()} telah ditambahkan ke keranjang.`);
+    const quantity = parseInt(document.getElementById(`quantity-${productName}`).value);
+
+    const productInCart = cart.find(product => product.name === productName);
+    if (productInCart) {
+        productInCart.quantity += quantity;
+    } else {
+        cart.push({
+            name: productName,
+            price: productPrice,
+            quantity: quantity
+        });
+    }
+    alert(`${productName} telah ditambahkan ke keranjang.`);
 }
+
+function orderDetail() {
+    hideAll();
+    const detailsList = document.getElementById('orderDetailsList');
+    detailsList.innerHTML = "";
+
+    let total = 0;
+    cart.forEach(product => {
+        const productTotal = product.price * product.quantity;
+        detailsList.innerHTML += `<p>${product.name} (IDR ${product.price.toLocaleString()} x ${product.quantity}) = IDR ${productTotal.toLocaleString()}</p>`;
+        total += productTotal;
+    });
+
+    document.getElementById('totalPayment').innerText = total.toLocaleString();
+    document.getElementById('orderDetail').style.display = 'block';
+}
+
+function generateInvoice() {
+    hideAll();
+
+    const invoiceNumber = `INV${new Date().getTime()}`;
+    document.getElementById('invoiceNumber').innerText = invoiceNumber;
+
+    const invoiceDetailsList = document.getElementById('invoiceDetailsList');
+    invoiceDetailsList.innerHTML =
